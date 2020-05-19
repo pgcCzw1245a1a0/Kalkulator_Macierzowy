a46a3fd6979aedbc6a1e24eae14802371ce23b68
SzyM0 <szymixx12500@gmail.com>
2020-05-19 17:10:27 +0200

    Arytmetyka
    Wprowadziłem trochę zamieszania ale już

--- Python_implementation/Matrix_class_def.py
+++ Python_implementation/Matrix_class_def.py
@@ -7,10 +7,9 @@ class Matrix:
         self.matrix = array(matrix)
 
     def dim(self):  #return dimention of matrix [rows x columns]
-        return self.matrix.ndim
-    def shape(self): #return touple with the numer of columns and rows
         return self.matrix.shape
-    def transpose(self): #teturn
+
+    def transpose(self): #return transposed matrix
         return self.matrix.transpose()
 
 
