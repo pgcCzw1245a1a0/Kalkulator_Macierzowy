ad2bb6af57e57bcb51755d68bf40869b6e9b842e
SzyM0 <szymixx12500@gmail.com>
2020-05-19 16:23:07 +0200

    Arytmetyka
    Usprawniłem klasę matrix, dodałem metody transpozycji i wymiaru, dodałem funkcje mnożenia macierzowego, dodawania i odejmowania

--- Python_implementation/Matrix_class_def.py
+++ Python_implementation/Matrix_class_def.py
@@ -1,13 +1,24 @@
 #TODO
+from numpy import *
 
 class Matrix:
 
     def __init__(self, matrix):
-        self.matrix = matrix
+        self.matrix = array(matrix)
 
     def dim(self):  #return dimention of matrix [rows x columns]
-        dimention = []
-        dimention.append(len(self.matrix))
-        dimention.append(len(self.matrix[0]))
+        return self.matrix.ndim
+    def shape(self): #return touple with the numer of columns and rows
+        return self.matrix.shape
+    def transpose(self): #teturn
+        return self.matrix.transpose()
 
-        return dimention
+
+def multiplication(matrix_1, matrix_2):
+    return matrix_1 @ matrix_2
+
+def addition(matrix_1, matrix_2):
+    return matrix_1 + matrix_2
+
+def subrtaction(matrix_1, matrix_2):
+    return matrix_1 - matrix_2
