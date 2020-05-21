8d17c1ba22c558fe723331e003963c9df13514e2
GacinhoV33 <fgacek@student.agh.edu.pl>
2020-05-21 21:45:44 +0200

    dodanie komentarzy do savefunctions, wypelnienie dokumentacji

--- Python_implementation/Savefunctions.py
+++ Python_implementation/Savefunctions.py
@@ -6,33 +6,31 @@ import json, os, path
 import numpy as np
 
 
-def save_operation(matrix1, matrix2, sign, result, counter):
+def save_operation(matrix1, matrix2, sign, result, counter):                         #operacja zapisywania, przyjmuje macierze, działanie, macierz wynikową i licznik)
     prepare_memory(counter)
-    operation_dict = {"mat1": matrix1.tolist(),
+    operation_dict = {"mat1": matrix1.tolist(),                                      #tworzenie słownika
                       "mat2": matrix2.tolist(),
                       "sign": sign,
                       "res": result.tolist()}
-    filename = "./memory/operation_memory_{}.json".format(counter)
+    filename = "./memory/operation_memory_{}.json".format(counter)                   #zamiana słownika na json_dict
     json_dict = json.dumps(operation_dict)
     with open(filename,"w") as file:
         file.write(json_dict)
 
 
-def load_operation(cnt):
-    filename = "./memory/operation_memory_{}.json".format(cnt)
+def load_operation(cnt):    #funkcja otrzymuje licznik, ktory okresla ktora macierz chcemy wczytac
+    filename = "./memory/operation_memory_{}.json".format(cnt)   #koncowka (cnt) okresla ktora macierz chcemy zaladowac
     with open(filename, "r") as file:
-        file_content = file.read()
-        print(file_content)
-        op_dict = json.loads(file_content)
-        print(op_dict['mat1'])
+        file_content = file.read()                                                  #wczytanie pliku
+        op_dict = json.loads(file_content)                                          #zamienienie jsona na format do odczytu
         return op_dict
 
 
 def prepare_memory(counter):
-    if not os.path.exists("./memory"):
-        os.mkdir("./memory")
+    if not os.path.exists("./memory"):                                              #przy pierwszym zadaniu tworzy folder memory
+        os.mkdir("./memory")                                                        #^^^^
     if counter > 10:
-        os.remove("./memory/operation_memory_{}.json".format(counter - 10))
+        os.remove("./memory/operation_memory_{}.json".format(counter - 10))         #Jeśli jest więcej niż 10 zadań to usuwane jest najstarsze
 
 # mat1 = np.array([[2,2], [2,3]])
 # mat2 = np.array([[2,2], [2,4]])
